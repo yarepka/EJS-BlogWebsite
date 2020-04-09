7f2e854ea866628164c04634a22ef41d09af376f
Роман Бубнов <yarepka01@gmail.com>
2020-04-09 14:30:40 +0300

    Updated app.js: added get methods for about and contact pages. Updated about and contact pages

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.get("/", (req, res) => {
   res.render("home", {homeStartingContent: homeStartingContent});
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {contactContent: contactContent});
+});
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
